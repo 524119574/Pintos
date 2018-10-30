ecf6251c560a48e578634229963977f14edfaee6
Wang Ge <wg116@sprite37.doc.ic.ac.uk>
2018-10-30 13:16:32 +0000

    set up system call infra

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,106 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "lib/syscall-nr.h"
 
 static void syscall_handler (struct intr_frame *);
+
+
+/* A system call. */
+struct system_call {
+  int argc;                        /* # of args, including action name. */
+  void (*function)(char **argv);   /* Function to execute action. */
+};
+
+/* The idea here is to create an array of this, and the first element
+ * in the array corresponding to the SYS_HALT, second to SYS_EXIT and so on.
+ * Basically one can expect to use */
+static void
+write_helper(int fd, const void *buffer, unsigned size) {
+  printf("in the helper.\n");
+  printf("fd is %d\n", fd);
+}
+
+static void
+halt(char **argv) {
+}
+
+static void
+exit(char **argv) {
+}
+
+static void
+exec(char **argv) {
+}
+static void
+wait(char **argv) {
+}
+static void
+create(char **argv) {
+}
+static void
+remove(char **argv) {
+}
+static void
+open(char **argv) {
+}
+static void
+file_size(char **argv) {
+}
+static void
+read(char **argv) {
+}
+
+// argv should be void pointers
+// this function will dereference the pointer, at this stage all pointer should
+// be valid.
+static void
+write(char **argv) {
+  int fd = *((int *) argv[0]);
+  const void *buffer = argv[1];
+  unsigned size = *((unsigned *) argv[2]);
+  write_helper(fd, buffer, size);
+}
+
+static void
+seek(char **argv) {
+}
+
+static void
+tell(char **argv) {
+}
+
+static void
+close(char **argv) {
+}
+
+static const struct system_call actions[] =
+    {
+        {1, halt},  // halt
+        {1, exit},  // exit
+        {1, exec},  // exec
+        {1, wait},  // wait
+        {2, create},  // create
+        {1, remove},  // remove
+        {1, open},  // open
+        {1, file_size},  // file_size
+        {3, read},  // read
+        {3, write},  // write
+        {2, seek},  // seek
+        {1, tell},  // tell
+        {1, close},  // close
+        {NULL, 0, NULL},
+    };
+
+static void **getArgs(int num, void *esp) {
+  void **args = malloc(sizeof(void *) * num);
+
+  for (int i = 0; i < num; i++) {
+    args[i] = (esp - (1 + i) * 4);
+    printf("pointers:%p\n", args[i]);
+  }
+  return args;
+}
 
 void
 syscall_init (void) 
@@ -16,5 +114,12 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
+  printf ("num: %li\n", *((long *) f->esp));
+  printf ("num: %d\n", *((int *) f->esp) - 1);
+  printf ("original esp pointers:%p\n", f->esp);
+
+  struct system_call system_call = actions[*((long *) f->esp)];
+  void **args = getArgs(system_call.argc, f->esp);
+  system_call.function(args);
   thread_exit ();
 }
